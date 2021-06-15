0974cb9c6b10e92e27281bfb7b4c223092d70811
ashourmoaaz <ashour.m@live.de>
2021-06-15 16:10:52 +0200

    create UserManager class

--- app/core/models.py
+++ app/core/models.py
@@ -2,3 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+class UserManager(BaseUserManager):
+    pass        
