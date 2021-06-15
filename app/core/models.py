33485d77273523b0d045848c5cade8ccace27bf7
ashourmoaaz <ashour.m@live.de>
2021-06-15 16:21:04 +0200

    create user model

--- app/core/models.py
+++ app/core/models.py
@@ -12,3 +12,9 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+        Custom user model that supports using email instead of username
+    """
+    pass
