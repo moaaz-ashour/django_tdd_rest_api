5264ffaa3e96f350508bb701b282e499dee1e350
ashourmoaaz <ashour.m@live.de>
2021-06-17 10:47:31 +0200

    register custom user model in Django Admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
 
-# Register your models here.
+admin.site.register(models.User, UserAdmin)
