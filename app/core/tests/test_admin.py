ef94b864c0999d288591a7660067a0fb769ee826
ashourmoaaz <ashour.m@live.de>
2021-06-17 11:10:52 +0200

    test admin user change page renders correctly

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -35,3 +35,11 @@ class AdminSiteTests(TestCase):
         # name field is not available in default UserAdmin class
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that user edit page works
+        """
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
