ba139b064789087ac2f01d0c19d2370da596b2b0
ashourmoaaz <ashour.m@live.de>
2021-06-24 13:46:36 +0200

    API to generate authentication tokens: - API implementation (serializer)

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,8 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+
+# Wrap the texts with this if you want django to automatically translate
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -28,3 +31,42 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it
         """
         return get_user_model().objects.create_user(**validated_data)
+    
+class AuthTokenSerializer(serializers.Serializer):
+    """
+        Serializer for user authentication object:
+        - Serializer can also be used without a model
+        - Since this is not a model, we inherit from serializers.Serializer
+        - We will get some data from user, validate it and return some value
+    """
+    # create fields to get data for authentication
+    email = serializers.CharField()
+    password = serializers.CharField(
+                style={'input_type': 'password'},
+                trim_whitespace=False
+                )
+
+    def validate(self, attrs):
+        """ override validate method and raise exception if invalid
+            it is called when we validate our serializers
+            attrs: every field which makes up the serializer
+        """
+        # attrs contains all the serializer fields defined above
+        # retrieve email and password from attrs
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+                request=self.context.get('request'),
+                username=email,
+                password=password
+            )
+        # if authentication fails:
+        if not user:
+            # we use gettext to enable language tranlation for this text
+            msg = _("Unable to authenticate with credentials provided")
+            # raise the relavant http status code
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
