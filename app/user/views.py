ce448404a63fac1e73dc5788dd7a66eefffaec49
ashourmoaaz <ashour.m@live.de>
2021-06-24 13:47:27 +0200

    API to generate authentication tokens: - API implementation (view + URL)

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,18 @@
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user"""
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """view for API validating user credentials and providing token
+    """
+    serializer_class = AuthTokenSerializer
+    # class that will render this page
+    # works without this but does not create nice view in the browser
+    # as it did when extended from generic views
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
